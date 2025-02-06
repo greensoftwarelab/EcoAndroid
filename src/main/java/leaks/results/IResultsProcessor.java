af978dfb3f9a5fbb15d996fd16cd6a59e92b86ed
rrua <rui.rrua@gmail.com>
2025-02-06 22:32:54 +0400

    fix(gradle version and dependencies): updated project and added task to generate resource leaks jar

--- src/main/java/leaks/results/IResultsProcessor.java
+++ src/main/java/leaks/results/IResultsProcessor.java
@@ -1,6 +1,5 @@
 package leaks.results;
 
-import leaks.analysis.AllInOneRLAnalysis;
 import leaks.analysis.IFDSRLAnalysis;
 import leaks.analysis.RLAnalysis;
 import leaks.analysis.VascoRLAnalysis;
@@ -8,6 +7,5 @@ import leaks.analysis.VascoRLAnalysis;
 public interface IResultsProcessor {
     void visit(RLAnalysis analysis);
     void visit(VascoRLAnalysis analysis);
-    void visit(AllInOneRLAnalysis allInOneRLAnalysis);
     void visit(IFDSRLAnalysis ifdsrlAnalysis);
 }
